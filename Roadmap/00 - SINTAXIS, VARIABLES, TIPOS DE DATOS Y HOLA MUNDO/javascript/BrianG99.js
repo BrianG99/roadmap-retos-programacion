dcb297533138440ece526e0692de1d7d3fee937e
brian <brianogarciav@gmail>
2025-03-27 08:07:57 -0300

    Add BrianG99.js

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/BrianG99.js
@@ -0,0 +1,53 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+//comentario una linea
+
+/* comentario
+multilinea
+*/
+
+/** 
+*comentario
+*de
+*documentacion
+*/
+
+
+
+// Declarar variable y constante: const y let
+const x = 99;
+let area = 34;
+
+// Ya no se usa: var
+var g = 12;
+
+
+//Datos Primitivos
+
+//string
+let saludo = "Hola Moure";
+
+//number
+let entero = 2;
+let decimal = 1.2;
+
+//boleano
+let valido = true;
+let novalido = false;
+
+//null
+let dato = null;
+
+//undefined
+let SinValor;
+
+//bigint
+let numerosgrrandes = 12312321321;
+
+//symbol
+let id = Symbol(id);
+
+console.log("¡Hola, Javascript!");
+
+
+
